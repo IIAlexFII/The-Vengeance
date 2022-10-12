0424be7dcb1cdb43a83c811074820a0a258bc051
IIAlexFII <sofia.a.o.cruzferreira@gmail.com>
2022-10-12 16:47:11 +0100

    game source

---
+++ The Vengeance - Game source/Assets/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+    //public static int goldStored = PlayerPrefs.GetInt("gold");
+    public static LoginInfo loginInfo;
+  
+
+}
